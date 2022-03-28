b547f859e50bc8f1d3b81227648105841009606c
Oleg Zaicev <30oleg.zv94@gmail.com>
2022-03-28 13:59:34 +0300

    style: add hover effect(navbar)

--- 37. React-StartPage/src/Component/HeaderBody/Content/Content.tsx
+++ 37. React-StartPage/src/Component/HeaderBody/Content/Content.tsx
@@ -4,7 +4,7 @@ import Button from '../Button/Button';
 const Content = () => {
   return (
     <div className="Header-content">
-      <div className="Header-title">New Automation Tool for Your Home</div>
+      <h1 className="Header-title">New Automation Tool for Your Home</h1>
       <div className="Header-subtitle">
         Lorem ipsum dolor sit amet, consectetur adipiscing <br />
         elit. Faucibus tristique vulputate ultrices ut mauris <br />
