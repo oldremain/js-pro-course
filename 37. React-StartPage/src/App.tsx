70e50ef7976e635aabed0dd68eaa2426981f6198
Oleg Zaicev <30oleg.zv94@gmail.com>
2022-03-30 14:07:14 +0300

    refactor: change styles extension, modifiy app.scss

--- 37. React-StartPage/src/App.tsx
+++ 37. React-StartPage/src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css';
+import './App.scss';
 import Header from './Component/Header/Header';
 import HeaderBody from './Component/HeaderBody/HeaderBody';
 
 function App() {
   return (
     <>
-      <Header />
-      <HeaderBody />
+      <header>
+        <div className="container">
+          <Header />
+          <HeaderBody />
+        </div>
+      </header>
+      <section>
+        <div className="container"></div>
+      </section>
     </>
   );
 }
