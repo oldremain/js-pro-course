9e0aa8bfed52441cca87308fd77bef799d48ae97
Oleg Zaicev <30oleg.zv94@gmail.com>
2022-04-17 14:17:23 +0300

    make header navigation fixed, change media queries

--- React-app/src/Component/Header/Logo/Logo.tsx
+++ React-app/src/Component/Header/Logo/Logo.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './Logo.scss';
-import logo from '../../../assets/img/logo.jpg';
+import "./Logo.scss";
+import logo from "../../../assets/img/logo.jpg";
 
 const Logo: React.FC = () => {
-  return (
-    <a href="/" className="Header-logo ">
-      <img src={logo} alt="logo" />
-    </a>
-  );
+    return (
+        <a href="/" className="Header-logo ">
+            <img src={logo} alt="logo" />
+            <div className="Logo-name">Atomic</div>
+        </a>
+    );
 };
 
 export default Logo;
