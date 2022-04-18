e012b71c57ec9b4e8e35f3b01b50f693c7de373a
Oleg Zaicev <30oleg.zv94@gmail.com>
2022-04-18 12:57:20 +0300

    add Image component for PostId component

--- React-app/src/Component/PostId/PostId.tsx
+++ React-app/src/Component/PostId/PostId.tsx
@@ -1,24 +1,16 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import useSinglePost from "../hooks/useSinglePost";
+import Image from "../PostImage/Image";
 import Loader from "../Posts/Loader/Loader";
 import Error from "../Posts/Error/Error";
 
 import "./PostId.scss";
-import notFoundImage from "../../assets/img/notfound.png";
 
 const PostId: React.FC = () => {
     const { id } = useParams();
 
-    const { post, loading, error, setError } = useSinglePost(id);
-
-    const handleError = () => {
-        setError({
-            status: true,
-            name: error.name,
-            message: error.message,
-        });
-    };
+    const { post, loading, error } = useSinglePost(id);
 
     if (loading) {
         return <Loader />;
@@ -32,13 +24,7 @@ const PostId: React.FC = () => {
 
     return (
         <Link to={`/posts/${id}`} className="Post-container">
-            <div className="Image">
-                {!!post.image && !error.status ? (
-                    <img src={post.image} onError={handleError} alt="card" />
-                ) : (
-                    <img src={notFoundImage} alt="notfound" />
-                )}
-            </div>
+            <Image data={post} />
             <div className="Title">{post.title}</div>
             <div className="Text">{post.text}</div>
             <div className="Date">{post.date}</div>
