cd8147cd19b49639434aa6d19d8d7765770d40c1
Oleg Zaicev <30oleg.zv94@gmail.com>
2022-04-27 14:02:57 +0300

    add redux npm package

--- React-app/src/index.tsx
+++ React-app/src/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
 
-import './index.scss';
+import "./index.scss";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
